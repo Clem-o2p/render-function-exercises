9f9e0c213cbb2cc8fb6855f182b9fca87df939d1
Clément Ollivier <clement.o2p@gmail.com>
2020-09-13 15:29:24 +0200

    :light_bulb: Solution

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
+/* Solution with string*/
+
 export const render = (renderContext, element, children) => {
-  return;
+  const domElement = renderContext.createElement(element);
+
+  if (children) {
+    domElement.insertAdjacentHTML("beforeend", children);
+  }
+
+  return domElement.outerHTML;
 };
 
-export const mount = (renderContext, el, component) => {};
+export const mount = (renderContext, el, component) => {
+  const rootElement = renderContext.getElementById(el);
+  rootElement.outerHTML = component;
+}
+
+/* Solution with array */
+
+// export const render = (renderContext, element, children) => {
+//   const domElement = renderContext.createElement(element);
+
+//   if (children) {
+//     if (typeof children === "string") {
+//       domElement.insertAdjacentHTML("beforeend", children);
+//     } else if (typeof children === "object" && children.length) {
+//       children.forEach(child => {
+//         domElement.insertAdjacentHTML("beforeend", child);
+//       })
+//     } else {
+//       console.log("children param must be a String or Array of Strings");
+//       return;
+//     }
+//   }
+
+
+//   return domElement.outerHTML;
+// };
+
+// export const mount = (renderContext, el, component) => {
+//   const rootElement = renderContext.getElementById(el);
+//   rootElement.outerHTML = component;
+// }
