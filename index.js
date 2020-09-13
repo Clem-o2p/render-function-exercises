2e63d6e0a96be5a58ff03affc433e44c15ca3b5a
Clément Ollivier <clement.o2p@gmail.com>
2020-09-13 21:59:15 +0200

    :sparkles: Add attributes to components

--- index.js
+++ index.js
@@ -1,38 +1,11 @@
 /* Solution with string*/
 
-export const render = (renderContext, element, children) => {
-  const domElement = renderContext.createElement(element);
-
-  if (children) {
-    domElement.insertAdjacentHTML("beforeend", children);
-  }
-
-  return domElement.outerHTML;
-};
-
-export const mount = (renderContext, el, component) => {
-  const rootElement = renderContext.getElementById(el);
-  rootElement.outerHTML = component;
-}
-
-/* Solution with array */
-
 // export const render = (renderContext, element, children) => {
 //   const domElement = renderContext.createElement(element);
 
 //   if (children) {
-//     if (typeof children === "string") {
-//       domElement.insertAdjacentHTML("beforeend", children);
-//     } else if (typeof children === "object" && children.length) {
-//       children.forEach(child => {
-//         domElement.insertAdjacentHTML("beforeend", child);
-//       })
-//     } else {
-//       console.log("children param must be a String or Array of Strings");
-//       return;
-//     }
+//     domElement.insertAdjacentHTML("beforeend", children);
 //   }
-
 
 //   return domElement.outerHTML;
 // };
@@ -41,3 +14,36 @@ export const mount = (renderContext, el, component) => {
 //   const rootElement = renderContext.getElementById(el);
 //   rootElement.outerHTML = component;
 // }
+
+/* Solution with array */
+
+export const render = (renderContext, element, attributes, children) => {
+  const domElement = renderContext.createElement(element);
+
+  if (attributes) {
+    Object.keys(attributes).forEach(attrKey => {
+      domElement.setAttribute(attrKey, attributes[attrKey])
+    })
+  }
+
+  if (children) {
+    if (typeof children === "string") {
+      domElement.insertAdjacentHTML("beforeend", children);
+    } else if (typeof children === "object" && children.length) {
+      children.forEach(child => {
+        domElement.insertAdjacentHTML("beforeend", child);
+      })
+    } else {
+      console.log("children param must be a String or Array of Strings");
+      return;
+    }
+  }
+
+
+  return domElement.outerHTML;
+};
+
+export const mount = (renderContext, el, component) => {
+  const rootElement = renderContext.getElementById(el);
+  rootElement.outerHTML = component;
+}
--- index.test.js
+++ index.test.js
@@ -10,9 +10,9 @@ const mockRenderContext = dom.window.document;
 
 /* Tests begin */
 
-const HelloTitle = render(mockRenderContext, "h1", "Hello");
+const HelloTitle = render(mockRenderContext, "h1", null, "Hello");
 
-const Paragraph = render(mockRenderContext, "p", "lorem ipsum dolor sit amet");
+const Paragraph = render(mockRenderContext, "p", null, "lorem ipsum dolor sit amet");
 
 test("Render an empty <div>", () => {
   expect(render(mockRenderContext, "div")).toStrictEqual("<div></div>");
@@ -23,9 +23,13 @@ test("Render an h1 tag saying 'Hello'", () => {
 });
 
 test("Render a div tag containing an h1 saying 'Hello'", () => {
-  expect(render(mockRenderContext, "div", HelloTitle)).toStrictEqual("<div><h1>Hello</h1></div>");
+  expect(render(mockRenderContext, "div", null, HelloTitle)).toStrictEqual("<div><h1>Hello</h1></div>");
 });
 
 test("Render a div tag containing an h1 saying 'Hello' and a paragraph of dummy text", () => {
-  expect(render(mockRenderContext, "div", [HelloTitle, Paragraph])).toStrictEqual("<div><h1>Hello</h1><p>lorem ipsum dolor sit amet</p></div>");
+  expect(render(mockRenderContext, "div", null, [HelloTitle, Paragraph])).toStrictEqual("<div><h1>Hello</h1><p>lorem ipsum dolor sit amet</p></div>");
+});
+
+test("Render a div tag with id 'main' and the class 'container', containing an h1 saying 'Hello'", () => {
+  expect(render(mockRenderContext, "div", {id: "main", class: "container"}, HelloTitle)).toStrictEqual(`<div id="main" class="container"><h1>Hello</h1></div>`);
 });
